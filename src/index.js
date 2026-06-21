dc3d211c8a0378081f4b01ffb9d1163226588e53
Abhinav053 <ranjanbhinav392@gmail.com>
2026-06-22 00:38:23 +0530

    idempotency keys saved in redis-2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 const express = require('express');
 
 const { ServerConfig, Queue } = require('./config');
+const redisClient = require('./config/redis');
 const apiRoutes = require('./routes');
 const CRON = require('./utils/common/cron-jobs');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes);
 app.use('/bookingService/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    CRON();
-    await Queue.connectQueue();
-    console.log("queue connected")
+    try {
+        console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
+
+        await redisClient.connect();
+        console.log("Redis connected");
+
+        await Queue.connectQueue();
+        console.log("Queue connected");
+
+        CRON();
+    } catch (err) {
+        console.error("Server startup failed:", err);
+    }
 });
